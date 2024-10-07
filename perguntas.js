ebe732c584e257e5fde655eef5a30d0d123204fd
lubotn <142837650+lubotn@users.noreply.github.com>
2024-10-07 12:15:27 -0300

    vvvv

---
+++ perguntas.js
@@ -0,0 +1,96 @@
+criacartao(
+'Matemática',
+'Qual é a fórmula para calcular a área de um triângulo?',
+'Resposta: A = b x h / 2',
+)
+criacartao(
+'História',
+'Qual é a capital do Império Romano durante a maior parte da sua história?',
+'Resposta: Roma',
+)
+criacartao(
+'Física',
+'O que é a Lei da Gravitação de Newton?',
+'Resposta: A força de atração entre dois corpos é diretamente proporcional ao produto de suas massas.',
+)
+criacartao(
+'Biologia',
+'Qual é a principal função dos ribossomos nas células?',
+'Resposta: Sintetizar proteínas',
+)
+criacartao(
+'Matemática',
+'Qual é a equação geral da reta no plano cartesiano?',
+'Resposta: y = mx + b',
+)
+criacartao(
+'Língua Portuguesa',
+'Qual é a principal obra de Machado de Assis que faz parte do movimento realista?',
+'Resposta: Dom Casmurro',
+)
+criacartao(
+'História',
+'Em que ano foi proclamada a independência do Brasil?',
+'Resposta: 1822',
+)
+criacartao(
+'Física',
+'Qual é a fórmula da velocidade média?',
+'Resposta: v = s/t',
+)
+criacartao(
+'Biologia',
+'Qual é o principal processo de divisão celular em organismos eucarióticos?',
+'Resposta: Mitose',
+)
+criacartao(
+'Química',
+'Qual é o nome dado ao fenômeno de expansão dos gases quando a temperatura aumenta?',
+'Resposta: Lei de Charles',
+)
+criacartao(
+'matemática',
+'Qual é a raiz quadrada de 144?',
+'Resposta: 12',
+)
+criacartao(
+'matemática',
+'Qual é o valor de 3x+5=20',
+'Resposta: x=5.',
+)
+criacartao(
+'História',
+'Em que ano aconteceu a Proclamação da República no Brasil?',
+'Resposta: 1889.',
+)
+criacartao(
+'História',
+'Qual foi o principal objetivo das Grandes Navegações do século XV?',
+'Resposta: Encontrar novas rotas comerciais para as Índias e expandir territórios.'
+)
+criacartao(
+: 'Geografia'
+15.Qual é o rio mais extenso do mundo? Resposta: Rio Amazonas.
+)
+criacartao(
+: 'Geografia'
+16.Em que hemisfério se encontra a maior parte do continente africano? Resposta: Hemisfério Oriental.
+)
+criacartao(
+: 'Ciências'
+17.Qual é o nome do processo pelo qual as plantas produzem seu próprio alimento usando luz solar? Resposta: Fotossíntese.
+)
+criacartao(
+: 'Ciências'
+18.Qual é o gás mais abundante na atmosfera terrestre? Resposta: Nitrogênio.
+)
+criacartao(
+: Língua Portuguesa
+)
+criacartao(
+19.Qual é a função de um advérbio em uma frase? Resposta: Modificar o verbo, o adjetivo ou outro advérbio, indicando circunstâncias como tempo, lugar, modo, intensidade, etc.
+)
+criacartao(
+: 'Língua Portuguesa'
+20.Qual é o objeto direto na frase: "Maria leu o livro rapidamente"? Resposta: "o livro".
+)
